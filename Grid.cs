e82f6159b6e876f89aedb397112eca094d905b26
Ben <rbenrodford@gmail.com>
2019-11-14 22:17:27 -0500

    new basic models

--- Grid.cs
+++ Grid.cs
@@ -8,7 +8,6 @@ public class Grid : TileMap
 	// currently selected node/sprite
 	private Node2D selected = null;
 	
-	
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
 	{
