bc91d1f6aed6ca0b8d814d3810c0ed94e0c0d470
Robert <holfordr@mcmaster.ca>
2019-11-17 16:06:51 -0500

    Simply random movement of ship

--- Grid.cs
+++ Grid.cs
@@ -19,6 +19,7 @@ public class Grid : TileMap
 			GD.Print("Node loaded: ", child, " at ", WorldToMap(child.GetPosition()));
 		}
     }
+	private int gridSize = 32;
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
 //  public override void _Process(float delta)
@@ -44,7 +45,7 @@ public class Grid : TileMap
 				{
 					Node2D child = (Node2D)GetChild(i);
 					// if user clicked on a child
-					if (cell == WorldToMap(child.GetPosition()))
+					if (cell == WorldToMap(child.GetPosition()) && child !=  (Node2D)GetNode("../CompShip"))
 					{
 						if (this.selected == null)
 						{
@@ -82,6 +83,32 @@ public class Grid : TileMap
 					this.selected = null;
 				}
 			}
+		}
+	}
+	private void _on_CompTurn_pressed(){
+		Node2D compShip = (Node2D)GetNode("CompShip");
+		Vector2 shipCell = compShip.Position; 
+		Random r = new Random();
+		int randDirection = r.Next(0, 4); //0 = north, 1 east, 2 south, 3 west
+		//to do check ray cast for valid move, snapped, add collision with other ship
+		GD.Print("direction test:", randDirection);
+		switch (randDirection){
+			case 0:
+			    Vector2 moveNorth = new Vector2(shipCell.x, shipCell.y - 1 * gridSize);
+				compShip.SetPosition(moveNorth);
+				break;			
+			case 1:
+			    Vector2 moveEast = new Vector2(shipCell.x + 1 * gridSize, shipCell.y);
+				compShip.SetPosition(moveEast);
+				break;
+			case 2:
+			    Vector2 moveSouth = new Vector2(shipCell.x, shipCell.y + 1 * gridSize);
+				compShip.SetPosition(moveSouth);
+				break;			
+			case 3:
+			    Vector2 moveWest = new Vector2(shipCell.x - 1 * gridSize, shipCell.y);
+				compShip.SetPosition(moveWest);
+				break;
 		}
 	}
 }
