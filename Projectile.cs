7e08c8fcebdc0c13501b3350201a8a0e632e8bbc
unknown <nicholasmigliore999@gmail.com>
2020-01-20 15:17:12 -0500

    Added projectiles to ship1.cs, todo notes

--- Projectile.cs
+++ Projectile.cs
@@ -5,13 +5,13 @@ namespace shipClass
 {
     public class Projectile : Node2D
     {
-        public string name { get; set; } = "";//the name of the projectile
-        public int firepower { get; set; } = 0;//the firepower of the projectile
-        public int penetration { get; set; } = 0;//the penetration multiplier of the projectile
-        public int accuracy { get; set; } = 0;//the accuracy multiplier of the projectile
-        public int range { get; set; } = 0;//the range of the weapon in tiles
-        public int APcost { get; set; } = 0;//the action point cost
-        public string type { get; set; } = "";//the type of weapon it is
+        public string name = "";//the name of the projectile
+        public int firepower = 0;//the firepower of the projectile
+        public int penetration = 0;//the penetration multiplier of the projectile
+        public int accuracy = 0;//the accuracy multiplier of the projectile
+        public int range = 0;//the range of the weapon in tiles
+        public int APcost = 0;//the action point cost
+        public string type = "";//the type of weapon it is, some types of weapons will be more effective against certain foes
 		
 		//constructor with parameters
         public Projectile(string nam, int fp, int pen, int acc, int ran, int ap, string typ)
@@ -40,6 +40,31 @@ namespace shipClass
         {
             firepower = fp;
         }
+		public int getPen()
+        {
+            return penetration;
+        }
+		public int getAcc()
+        {
+            return accuracy;
+        }
+		public int getRange()
+        {
+            return range;
+        }
+		public int getAPCost()
+        {
+            return APcost;
+        }
+		public string getName()
+        {
+            return name;
+        }
+		public string getType()
+        {
+            return type;
+        }
+		
 
         // Called when the node enters the scene tree for the first time.
         public override void _Ready()
@@ -50,7 +75,8 @@ namespace shipClass
 
     }
 
-/*    public class Weapons : Node2D
+//I tried to define a class of weapons but it will not let me reference them at all. I hope to properly define the weapons as described here
+    public class Weapons : Node2D
     {
         public Projectile Gun = new Projectile("Gun", 1, 2, 2, 8, 1, "normal");
 		public Projectile Missile = new Projectile("Missile", 2, 3, 2, 10, 2, "solid");
@@ -75,7 +101,7 @@ namespace shipClass
 
         }
     }
-*/
+
     
     
     
--- Ship1.cs
+++ Ship1.cs
@@ -16,13 +16,20 @@ public class Ship1 : Node2D
     private int maxAP = 4;//the maximum action points of a ship, it will reset to this value at the start of every turn
     private int range = 5;//the range it can move
 	
-	public shipClass.Projectile weapon1 { get; set; } = new shipClass.Projectile("Gun", 1, 1, 1, 8, 1, "normal");//the first weapon that the ship has
+	public shipClass.Projectile weapon1 { get; set; } = new shipClass.Projectile("Gun", 1, 2, 2, 8, 1, "normal");//the first weapon that the ship has
 	//public shipClass.Projectile weapon1 { get; set; } = new shipClass.Weapons.Gun;//the first weapon that the ship has
+	public shipClass.Projectile weapon2 = new shipClass.Projectile("Missile", 2, 3, 2, 10, 2, "solid");//the second weapon a ship has
+	public shipClass.Projectile weapon3 = new shipClass.Projectile("Laser", 2, 2, 3, 10, 2, "shiny");//the third weapon a ship has
+
 
     public int GetRange()
     {
         return range;
     }
+	
+	//TODO: add proper getter and setter functions for each of the ships variables
+	//TODO: add the ability to select weapons when attacking
+	//TODO: make the calculation in take_damage more complex, factoring in the defenders armour/evasion and the attackers penetration/accuracy
 
     //these values serve as base values for weapons and skills
     //we will most likely change every value here later on
