505cbfb98383a85741cfb2f67ab817eee3512a79
Robert Rodford <rbenrodford@gmail.com>
2020-04-26 11:54:15 -0400

    indentation

--- State.cs
+++ State.cs
@@ -11,37 +11,42 @@ public class State : Node
   public static int currentLevel {get; set;} = 1;
   public static void nextLevel()
   {
-	// increment state variables to unlock next level
-	State.maxLevel = Math.Max(State.maxLevel, State.currentLevel + 1);
-	State.currentLevel += 1;
+    // increment state variables to unlock next level
+    State.maxLevel = Math.Max(State.maxLevel, State.currentLevel + 1);
+    State.currentLevel += 1;
   }
   private static Dictionary<string, object> Savestate()
   {
-	return new Dictionary<string, object>()
-	{
-	  {"maxLevel", maxLevel},
-	  {"date", DateTime.Now}
-	};
+    return new Dictionary<string, object>()
+    {
+      {"maxLevel", maxLevel},
+      {"date", DateTime.Now}
+    };
   }
 
+  /* Save the current state to file
+  */
   public static bool Save()
   {
-	try {
-	  File saveFile = new File();
-	  saveFile.Open("sav.sd", File.ModeFlags.Write);
+    try {
+      File saveFile = new File();
+      saveFile.Open("sav.sd", File.ModeFlags.Write);
 
-	  Dictionary<string, object> saveData = Savestate();
+      Dictionary<string, object> saveData = Savestate();
 
-	  saveFile.StoreLine(JSON.Print(saveData));
+      saveFile.StoreLine(JSON.Print(saveData));
 
-	  saveFile.Close();
-	} catch (System.Exception e) {
-	  GD.Print(e.ToString());
-	  return false;
-	}
-	return true;
+      saveFile.Close();
+    } catch (System.Exception e) {
+      GD.Print(e.ToString());
+      return false;
+    }
+    return true;
   }
 
+  /* load a savestate from file
+  * @returns true if loaded sucessfully, false if errors
+  */
   public static bool Load()
   {try{
 	var saveFile = new File();
@@ -51,9 +56,7 @@ public class State : Node
 	saveFile.Open("sav.sd", File.ModeFlags.Read);
 
 	while (!saveFile.EofReached())
-	{	  
-	GD.Print(JSON.Parse(saveFile.GetLine()).Result.GetType());
-
+	{
 	  var currentLine = (Dictionary<object, object>)JSON.Parse(saveFile.GetLine()).Result;
 	  if (currentLine == null)
 		continue;
@@ -64,17 +67,17 @@ public class State : Node
 		string key = entry.Key.ToString();
 		switch (key)
 		{
-		case "maxLevel":
-		  maxLevel = (int)entry.Value;
-		  currentLevel = maxLevel;
-		  break;
-		}
+      case "maxLevel":
+        maxLevel = (int)entry.Value;
+        currentLevel = maxLevel;
+        break;
+      }
 	  }
 	}
 	saveFile.Close();
 	return true;
   } catch (System.Exception e) {
-	GD.Print(e.ToString());
-	return false;
+    GD.Print(e.ToString());
+    return false;
   }}
 }
