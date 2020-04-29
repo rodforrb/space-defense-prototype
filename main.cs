1bac3bbf2e4e2428dedf0cea91ad9cf4cde5dd11
Robert Rodford <rbenrodford@gmail.com>
2020-04-28 23:23:59 -0400

    removed extra files

--- main.cs
+++ main.cs
@@ -1,27 +1,3 @@
+/* This file is here because main.cs needs to exist */
 using Godot;
-using System;
-
-public class main : Node2D
-{
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-			
-	}
-	/*
-	public void _on_Exit_gui_input(InputEvent @event){
-		if (@event is InputEventMouseButton mbe && mbe.ButtonIndex == (int)ButtonList.Left && mbe.Pressed)
-	{
-		GetTree().ChangeScene("res://main.tscn");
-		GD.Print("Exit game clicked");
-	}
-	}
-	*/
-}	
-
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
-
+public class main : Node2D {}
